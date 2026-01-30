403299562aed033857be599c00d8401292337ef9
Jame Zhan <jame@orderly.network>
2026-01-31 01:35:18 +0800

    feat: update leaderboard page

--- src/app/[lang]/leaderboard/view.tsx
+++ src/app/[lang]/leaderboard/view.tsx
@@ -1,23 +1,11 @@
 "use client";
 
-import { useMemo } from "react";
-import { i18n, parseI18nLang } from "@orderly.network/i18n";
-import { LeaderboardPage } from "@orderly.network/trading-leaderboard";
-import { PathEnum } from "@/constant";
-import { getSymbol } from "@/storage";
+import { GeneralLeaderboardWidget } from "@orderly.network/trading-leaderboard";
 
 export default function LeaderboardView() {
-  const tradingUrl = useMemo(() => {
-    const symbol = getSymbol();
-    return `/${parseI18nLang(i18n.language)}${PathEnum.Perp}/${symbol}`;
-  }, []);
-
   return (
-    <LeaderboardPage
-      href={{
-        trading: tradingUrl,
-      }}
-      className="oui-py-5"
-    />
+    <div className="oui-py-6 oui-px-4 lg:oui-px-6 xl:oui-pl-4 lx:oui-pr-6">
+      <GeneralLeaderboardWidget />
+    </div>
   );
 }
